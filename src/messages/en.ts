6844a2cdde4ae9d431b42e6dc2b08763470500e2
Donnevtis <nevtees@gmail.com>
2023-01-21 17:44:44 +0300

    add messages

---
+++ src/messages/en.ts
@@ -0,0 +1,77 @@
+export const greeting = `
+Hey there! I'm an emotion diary bot here to assist you in getting to know yourself and your emotions better. With me, you can track your feelings and share your story with a therapist.
+
+Here are some of the things you can do with me:
+
+- Send a weekly report by typing /weeklyreport
+- Send a monthly report by typing /monthlyreport
+- Change your reminder settings by typing /reminder
+
+If you have any issues or suggestions, just let me know by typing @hay_sup.
+`
+
+export const EMOTIONS = {
+  Enjoyment: [
+    'happiness',
+    'love',
+    'relief',
+    'contentment',
+    'amusement',
+    'joy',
+    'pride',
+    'excitement',
+    'peace',
+    'satisfaction',
+  ],
+  Sadness: [
+    'lonely',
+    'heartbroken',
+    'gloomy',
+    'disappointed',
+    'hopeless',
+    'grieved',
+    'unhappy',
+    'lost',
+    'troubled',
+    'resigned',
+    'miserable',
+  ],
+  Fear: [
+    'worried',
+    'doubtful',
+    'nervous',
+    'anxious',
+    'terrified',
+    'panicked',
+    'horrified',
+    'desperate',
+    'confused',
+    'stressed',
+  ],
+  Anger: [
+    'annoyed',
+    'frustrated',
+    'peeved',
+    'contrary',
+    'bitter',
+    'infuriated',
+    'irritated',
+    'mad',
+    'cheated',
+    'vengeful',
+    'insulted',
+  ],
+  Disgust: [
+    'dislike',
+    'revulsion',
+    'loathing',
+    'disapproving',
+    'offended',
+    'horrified',
+    'uncomfortable',
+    'nauseated',
+    'disturbed',
+    'withdrawn',
+    'aversion',
+  ],
+}
