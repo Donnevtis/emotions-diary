e7817f0d9cc01d7e6e6bdc31a2b980009ad32869
Donnevtis <nevtees@gmail.com>
2023-02-22 14:44:20 +0300

    change language source

--- src/replies/report.ts
+++ src/replies/report.ts
@@ -1,16 +1,15 @@
 import { Context } from 'telegraf'
 import { getReport } from '../api'
 import { errorHandler } from '../utils/common'
-import { t } from '../services/locale'
+import localeService, { t } from '../services/locale'
 
 const replyErrorHandler = errorHandler('Report reply exception.')
 
 export async function sendReportWeek(this: Context) {
   const id = this.from?.id
-  const language_code = this.from?.language_code
 
   try {
-    getReport(id, 'xlsx', language_code, {
+    getReport(id, 'xlsx', localeService.locale, {
       start: new Date().setDate(new Date().getDate() - 7),
     })
   } catch (error) {
@@ -22,10 +21,9 @@ export async function sendReportWeek(this: Context) {
 
 export async function sendReportMonth(this: Context) {
   const id = this.from?.id
-  const language_code = this.from?.language_code
 
   try {
-    getReport(id, 'xlsx', language_code, {
+    getReport(id, 'xlsx', localeService.locale, {
       start: new Date().setDate(new Date().getMonth() - 1),
     })
   } catch (error) {
